249fb693ae374a0575c4a33de3a37a1077b6e99e
richardm <rmengca@gmail.com>
2022-07-11 16:42:33 -0400

    Unsubscribe observable

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,21 +1,29 @@
 import { CounterState } from './../state/counter.state';
 import { Store } from '@ngrx/store';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit {
-  counter:number=0;
+export class CounterOutputComponent implements OnInit, OnDestroy {
+  counter:number;
+  counterSub:Subscription;
+
 
   constructor(private store:Store<{counter:CounterState}>) { }
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe (data=>{
+    this.counterSub = this.store.select('counter').subscribe ((data)=>{
         this.counter=data.counter;
     });
   }
 
+  ngOnDestroy(){
+    if(this.counterSub){
+      this.counterSub.unsubscribe();
+    }
+  }
 }
